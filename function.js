4de81553d880e5b8af2d2f02ceee455432a01452
sungmin414 <sungmin3231@gmail.com>
2020-10-23 22:30:38 +0900

    함수 퀴즈

--- function.js
+++ function.js
@@ -152,3 +152,24 @@ const simpleMultiply = (a,b) => {
 (function hello(){
     console.log('IIFE');
 })();
+
+// Quiz
+// function calculate(command, a,b)
+// command: add, substract, divide, multiply, remainder
+function calculate(command, a,b){
+    switch (command){
+        case 'add':
+            return a + b;
+        case 'substract':
+            return a - b;
+        case 'divide':
+            return a / b;
+        case 'multiply':
+            return a * b;
+        case 'remainder':
+            return a % b;
+        default:
+            throw Error('값을 넣어라')
+    }
+}
+console.log(calculate('add', 3,2));
